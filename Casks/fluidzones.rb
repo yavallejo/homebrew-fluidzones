36af3c4019b695c6a1667d9eab13cfffd8c03928
Yan Vallejo <yavallejor@gmail.com>
2026-08-09 16:46:23 -0500

    Bump fluidzones to 1.1.0

--- Casks/fluidzones.rb
+++ Casks/fluidzones.rb
@@ -1,6 +1,6 @@
 cask "fluidzones" do
-  version "1.0.4"
-  sha256 "3ba816fc099291383f2e9ac4770f5f66c3667722f0707f99e7e49c61673300bd"
+  version "1.1.0"
+  sha256 "309f12e10eb26fccd0b426c72c4c09c30c42932a00264746523248b69d62dfbe"
 
   url "https://github.com/yavallejo/fluidzones-releases/releases/download/v#{version}/FluidZones-#{version}-arm64.dmg"
   name "FluidZones"
